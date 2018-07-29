4e030e1ecc28e75ae383413c1b54bf125ffd8c7e
Alexandre Chakroun <alexandre.chakroun@gmail.com>
2018-07-29 21:17:22 +0200

    Added basic auth for documents upload api

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::DocumentsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :authenticate
 
   def create
     document = Document.new(documents_params)
@@ -11,6 +12,15 @@ class Api::V1::DocumentsController < ApplicationController
   end
 
   private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      resource = Admin.find_by_email(username)
+      if resource && resource.valid_password?(password)
+        sign_in :admin, resource
+      end
+    end
+  end
 
   def documents_params
     params.require(:document).permit(:category, :file)
