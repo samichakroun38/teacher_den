39048b56f00d98dbab3468061f1b9ed10ec0add7
Alexandre Chakroun <alexandre.chakroun@gmail.com>
2018-08-26 14:54:12 +0200

    Added support for email sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,14 @@ Rails.application.configure do
 
   #Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # ActionMailer::Base.smtp_settings = {
+  #   user_name: ENV["SENDGRID_USERNAME"],
+  #   password: ENV["SENDGRID_PASSWORD"],
+  #   domain: "em1678.mail.chakroun.eu",
+  #   address: "smtp.sendgrid.net",
+  #   port: 587,
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'sami.chakroun.eu' }
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "em1678.mail.chakroun.eu",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
